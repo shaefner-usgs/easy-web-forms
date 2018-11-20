368497f01070a7676750bd040853ad6c07fa3220
Scott Haefner <shaefner@usgs.gov>
2018-11-20 13:27:32 -0800

    bugfix: use correct case for PDO param

--- dist/conf/config.inc.php
+++ dist/conf/config.inc.php
@@ -10,12 +10,12 @@ $dbTable = 'test';
 
 /*
 $host = '';
-$dbName = '';
+$dbname = '';
 $username = '';
 $password = '';
 
 try {
-  $db = new PDO("mysql:host=$host;dbName=$dbName;charset=utf8", $username, $password);
+  $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
   echo 'ERROR: ' . $e->getMessage();

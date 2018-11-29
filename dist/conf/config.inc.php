18bb8aa1d536ac49c7a0f4e1ac7be99dccbac5db
Scott Haefner <shaefner@usgs.gov>
2018-11-29 10:49:08 -0800

    add css class to error msg

--- dist/conf/config.inc.php
+++ dist/conf/config.inc.php
@@ -18,7 +18,7 @@ try {
   $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
-  echo 'ERROR: ' . $e->getMessage();
+  print '<p class="error">ERROR: ' . $e->getMessage() . '</p>';
 }
 */
 

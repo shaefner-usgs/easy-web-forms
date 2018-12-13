e51677b999a3affdd66cb0836ba1db40dc5652fa
Scott Haefner <shaefner@usgs.gov>
2018-12-13 10:31:05 -0800

    add polyfill for el.closest()

--- dist/js/script.js
+++ dist/js/script.js
@@ -292,6 +292,31 @@ var Validator = function (options) {
 };
 
 
+/**
+ * Polyfill for Element.closest()
+ */
+if (!Element.prototype.matches) {
+  Element.prototype.matches = Element.prototype.msMatchesSelector ||
+    Element.prototype.webkitMatchesSelector;
+}
+
+if (!Element.prototype.closest) {
+  Element.prototype.closest = function(s) {
+    var el = this;
+    if (!document.documentElement.contains(el)) {
+      return null;
+    }
+    do {
+      if (el.matches(s)) {
+        return el;
+      }
+      el = el.parentElement || el.parentNode;
+    } while (el !== null && el.nodeType === 1);
+    return null;
+  };
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
   Validator({
     form: document.querySelector('section.form form')

764ba400cdca7f09409f9daca51fd6fe3d9d4835
Haefner, Scott <shaefner@igswmhwglte0731.local>
2021-03-25 19:24:17 +0000

    add no cache headers

--- dist/easy-web-forms.inc.php
+++ dist/easy-web-forms.inc.php
@@ -1,6 +1,9 @@
 <?php
 
 header('Content-Type: text/html; charset=utf-8');
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 
 // Config
 include_once __DIR__ . '/conf/config.inc.php';
